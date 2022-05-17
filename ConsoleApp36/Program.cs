3cabc6fef860f2c2bf9565f37c7f5a806a6a05bc
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-05-17 06:50:13 +0300

    Add project files.

---
+++ ConsoleApp36/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp36
+{
+
+
+    class Student
+    {
+        public Guid id;
+        public string FirstName;
+        public string LastName;
+        public string MiddleName;
+    }
+
+    internal class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
